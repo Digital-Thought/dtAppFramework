d59b45cd20f4a399bbd751e9c0817112ed4e2773
Matthew <matthew@digital-thought.org>
2023-01-06 10:49:54 +1100

    Initial commit

---
+++ src/dtAppFramework/_version.py
@@ -0,0 +1,8 @@
+_VERSION = '1.0.0'
+_MAINTAINER = 'Matthew @ Digital-Thought'
+_AUTHOR = 'Digital-Thought'
+_AUTHOR_EMAIL = 'matthew@digital-thought.org'
+_DESCRIPTION = "Framework for making Digital-Thought Apps"
+_TITLE = "dtAppFramework"
+_PROJECT_URL = ""
+_DOCS_URL = ""
